01cc3926b20c24b635a9c63e2ea075ca3bcaf155
Miqdad DIAGNE <miqdaddiagne@MacBook-Pro-de-Miqdad.local>
2025-02-12 03:55:43 +0100

    Ajout d'icones dans Asso France

--- frontend/src/Components/AssoOne/AssoOne.js
+++ frontend/src/Components/AssoOne/AssoOne.js
@@ -1,8 +1,25 @@
+import Header from "../Header/header";
+import {GiHotMeal} from 'react-icons/gi';
+
 function AssoOne () {
 return (
-    <>
-    
-    </>
+    <div className="AssoOne">
+        <div className="Branche francaise de l'association">
+            <h1>Presentation de la branche francaise de l'association</h1>
+                <div className="action-item">
+                    <GiHotMeal className="icon" />
+                    <p>Dejeuners dansants</p>
+                </div>
+                <div className="action-item">
+                    <GiHotMeal className="icon" />
+                    <p>Collectes de vetements</p>
+                </div>
+                <div className="action-item">
+                    <GiHotMeal className="icon" />
+                    <p>Concerts de charite</p>
+                </div>
+        </div>
+    </div>
 )
 }
 export default AssoOne;
